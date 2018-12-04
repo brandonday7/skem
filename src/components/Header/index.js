fd989bc1d1b7cf063bce5641820832f24d78eff3
Brandon Day <brandontday7@gmail.com>
2018-12-03 23:53:43 -0500

    created project, began landing header

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
+import "./header.css";
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="header-container">
+				<ReactPlayer
+					url="https://s3.us-east-2.amazonaws.com/autzu-team/drums.mp4"
+					playing
+					muted
+					loop
+					width="100vw"
+					height="100vh"
+				/>
+				<div className="absolute">
+					<div className="title-container">
+						<p className="header-title large">SKEM</p>
+						<p className="header-title medium">presents...</p>
+						<p className="header-title medium">Record_087</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Header;
