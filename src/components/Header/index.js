ab4d61c3c5127d4d6dd9bf5fb94d93030d43c8a2
Brandon Day <brandontday7@gmail.com>
2018-12-04 08:33:12 -0500

    changed font, resize video for landing page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,18 +5,16 @@ import "./header.css";
 class Header extends Component {
 	render() {
 		return (
-			<div className="header-container">
-				<ReactPlayer
-					url="https://s3.us-east-2.amazonaws.com/autzu-team/drums.mp4"
-					playing
-					muted
-					loop
-					width="100vw"
-					height="100vh"
-				/>
+			<div className="video-container">
+				<video loop autoPlay muted>
+					<source
+						src="https://s3.us-east-2.amazonaws.com/autzu-team/drums.mp4"
+						type="video/mp4"
+					/>
+				</video>
 				<div className="absolute">
 					<div className="title-container">
-						<p className="header-title large">SKEM</p>
+						<p className="header-title large">After Ours</p>
 						<p className="header-title medium">Record_087</p>
 					</div>
 				</div>
