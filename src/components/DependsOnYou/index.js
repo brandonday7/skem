4ef0a640d9b753e7eb9dde7320be1fd8ac22b277
Brandon Day <brandontday7@gmail.com>
2018-12-11 23:14:19 -0500

    wow finally all the styling stacking is looking fine

---
+++ src/components/DependsOnYou/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./depends.css";
+
+class DependsOnYou extends Component {
+	render() {
+		return <div className="depends-container" />;
+	}
+}
+
+export default DependsOnYou;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
+import DependsOnYou from "./components/DependsOnYou";
 
 const Fullpage = () => (
 	<div className="app">
 		<Header />
 		<Gallery />
+		<DependsOnYou />
 	</div>
 );
 
