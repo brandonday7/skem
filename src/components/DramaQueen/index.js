dc1021d221d8b9ee20a3b1c4c048760074711839
Brandon Day <brandontday7@gmail.com>
2018-12-30 19:28:46 -0500

    this is actually not looking too bad

--- src/components/DramaQueen/index.js
+++ src/components/DramaQueen/index.js
@@ -11,51 +11,75 @@ class DramaQueen extends Component {
 				<h1 className="drama-heading">Drama Queen</h1>
 				<div className="dq-lyrics-container">
 					<div className="dq-left-lyrics">
-						<p>The sky could be clean no</p>
-						<p>Nights not what it needs to be</p>
-						<p>And echo</p>
-						<p />
-						<p>Im sprawling in my sleep</p>
-						<p>Dreams not what it seems to be</p>
-						<p>Im alone</p>
-						<p />
-						<p>Still water blowing steam</p>
-						<p>Being off beyond belief</p>
-						<p>Ur such a woah</p>
-						<p />
-						<p>And if its drama Im the Queen</p>
-						<p>How it pays to be me</p>
-						<p>How Im broke</p>
-						<p />
-						<p>I talk to myself all the time</p>
-						<p>Oh sigh here he goes fighting foes in his mind</p>
-						<p>And what its like to have the air touch my arm </p>
-						<p>Oh dear you wont be harmed</p>
-						<p>Oh maybe I dont care and maybe thats alright</p>
+						<div className="verse">
+							<p className="line">The sky could be clean no</p>
+							<p className="line">Nights not what it needs to be</p>
+							<p className="line">And echo</p>
+						</div>
+						<div className="verse">
+							<p className="line">Im sprawling in my sleep</p>
+							<p className="line">Dreams not what it seems to be</p>
+							<p className="line">Im alone</p>
+						</div>
+						<div className="verse">
+							<p className="line">Still water blowing steam</p>
+							<p className="line">Being off beyond belief</p>
+							<p className="line">Ur such a woah</p>
+						</div>
+						<div className="verse">
+							<p className="line">And if its drama Im the Queen</p>
+							<p className="line">How it pays to be me</p>
+							<p className="line">How Im broke</p>
+						</div>
+						<div className="verse">
+							<p className="line">I talk to myself all the time</p>
+							<p className="line">
+								Oh sigh here he goes fighting foes in his mind
+							</p>
+							<p className="line">
+								And what its like to have the air touch my arm{" "}
+							</p>
+							<p className="line">Oh dear you wont be harmed</p>
+							<p className="line">
+								Oh maybe I dont care and maybe thats alright
+							</p>
+						</div>
 					</div>
 					<img src={machine} className="machine-image" />
 					<div className="dq-right-lyrics">
-						<p>A hot head on the scene</p>
-						<p>Stop and staring at me</p>
-						<p>Put on a show</p>
-						<p />
-						<p>This sky could be clean</p>
-						<p>How many sides to this leaf</p>
-						<p>Time to go</p>
-						<p />
-						<p>I talk to myself all the time</p>
-						<p>Oh sigh here he goes fighting foes in his mind</p>
-						<p>And what its like to have the air touch my arm </p>
-						<p>Oh dear you wont be harmed</p>
-						<p>Oh maybe I dont care and maybe thats alright</p>
-						<p />
-						<p className="credits">
-							Written and recorded by Brandon Day Ethan Gans Nicholas Vereschak
-							and Ryan Garbett Recording engineer Danny Paulson Produced by
-							Danny Paulson Brandon Day Guitar and vocals Ethan Gans Guitar
-							Nicholas Vereschak Drums Ryan Garbett Bass guitar Danny Paulson
-							Organ Recorded in The Shed Runnymede Street Summer 2018
-						</p>
+						<div className="verse">
+							<p className="line">A hot head on the scene</p>
+							<p className="line">Stop and staring at me</p>
+							<p className="line">Put on a show</p>
+						</div>
+						<div className="verse">
+							<p className="line">This sky could be clean</p>
+							<p className="line">How many sides to this leaf</p>
+							<p className="line">Time to go</p>
+						</div>
+						<div className="verse">
+							<p className="line">I talk to myself all the time</p>
+							<p className="line">
+								Oh sigh here he goes fighting foes in his mind
+							</p>
+							<p className="line">
+								And what its like to have the air touch my arm{" "}
+							</p>
+							<p className="line">Oh dear you wont be harmed</p>
+							<p className="line">
+								Oh maybe I dont care and maybe thats alright
+							</p>
+						</div>
+						<div>
+							<p className="credits">
+								Written and recorded by Brandon Day Ethan Gans Nicholas
+								Vereschak and Ryan Garbett Recording engineer Danny Paulson
+								Produced by Danny Paulson Brandon Day Guitar and vocals Ethan
+								Gans Guitar Nicholas Vereschak Drums Ryan Garbett Bass guitar
+								Danny Paulson Organ Recorded in The Shed Runnymede Street Summer
+								2018
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
