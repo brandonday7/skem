c6a3f4a59038d972cab85b74b0dc803fbaa6eeca
Brandon Day <brandontday7@gmail.com>
2019-02-14 15:34:28 -0500

    mapped next button

--- src/components/Jukebox/ArcadeMachine.js
+++ src/components/Jukebox/ArcadeMachine.js
@@ -24,6 +24,19 @@ class ArcadeMachine extends Component {
 					name: "pause", 
 					shape: "poly", 
 					coords: [255, 563, 276, 563, 276, 585, 255, 585]
+				},
+				{
+					name: "next", 
+					shape: "poly", 
+					coords: [
+						348, 565,
+						362, 565,
+						363, 559,
+						385, 571,
+						364, 585,
+						363, 579,
+						348, 579
+					]
 				}
 			]
 		}
