d60cba3e725b89f202d5444e8e0e46e4b017b084
Brandon Day <brandontday7@gmail.com>
2019-02-14 15:41:58 -0500

    click handler on buttons

--- src/components/Jukebox/ArcadeMachine.js
+++ src/components/Jukebox/ArcadeMachine.js
@@ -1,51 +1,47 @@
 import React, { Component } from "react";
 import ImageMapper from 'react-image-mapper';
 import frontView from "../../images/frontView.png";
+import { buttonCoords } from "./buttonCoords.js"
 
 class ArcadeMachine extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			trackIndex: 0,
-			height: window.innerHeight
+			height: window.innerHeight,
+			playing: false
 		};
 	}
+
+	newIndex = () => {
+		const {trackIndex} = this.state
+		if (trackIndex === 3) {
+			return 0
+		}
+		return trackIndex + 1
+	}
+
+	buttonClick = button => {
+		if (button === "play") {
+			this.setState({ playing: true })
+		} else if (button === "pause") {
+			this.setState({ playing: false })
+		} else if (button === "next") {
+			this.setState({trackIndex: this.newIndex()})
+		}
+	}
+
 	render() {
 		const { height } = this.state
-		const MAP_AREA = {
-			name: "buttons",
-			areas: [
-				{
-					name: "play", 
-					shape: "poly", 
-					coords: [303, 559, 303, 586, 342, 574]
-				},
-				{
-					name: "pause", 
-					shape: "poly", 
-					coords: [255, 563, 276, 563, 276, 585, 255, 585]
-				},
-				{
-					name: "next", 
-					shape: "poly", 
-					coords: [
-						348, 565,
-						362, 565,
-						363, 559,
-						385, 571,
-						364, 585,
-						363, 579,
-						348, 579
-					]
-				}
-			]
-		}
 
 		return (
-    <ImageMapper src={frontView} map={MAP_AREA} height={0.95*height}
-    	onMouseMove={x => null}
-    	onImageMouseMove={evt => console.log(evt.clientX, evt.clientY)}
-    />
+	    <ImageMapper 
+	    	src={frontView} 
+	    	map={buttonCoords} 
+	    	height={0.95*height}
+	    	onMouseMove={x => null}
+	    	onClick={evt => this.buttonClick(evt.name)}
+	    />
 		);
 	}
 }
---
+++ src/components/Jukebox/buttonCoords.js
@@ -0,0 +1,30 @@
+const buttonCoords = {
+	name: "buttons",
+	areas: [
+		{
+			name: "play", 
+			shape: "poly", 
+			coords: [303, 559, 303, 586, 342, 574]
+		},
+		{
+			name: "pause", 
+			shape: "poly", 
+			coords: [255, 563, 276, 563, 276, 585, 255, 585]
+		},
+		{
+			name: "next", 
+			shape: "poly", 
+			coords: [
+				348, 565,
+				362, 565,
+				363, 559,
+				385, 571,
+				364, 585,
+				363, 579,
+				348, 579
+			]
+		}
+	]
+}
+
+export { buttonCoords }
