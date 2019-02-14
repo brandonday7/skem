3a0ec1cbec842f598c312a0f3c274d7fd54a4381
Brandon Day <brandontday7@gmail.com>
2019-02-14 16:04:06 -0500

    found a nice alternative for increasing button size on hover

--- src/components/Jukebox/ArcadeMachine.js
+++ src/components/Jukebox/ArcadeMachine.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import ImageMapper from 'react-image-mapper';
-import frontView from "../../images/frontView.png";
 import { buttonCoords } from "./buttonCoords.js"
+import frontView from "../../images/frontView.png";
+import pause from "../../images/pause.png";
+import play from "../../images/play.png";
+import next from "../../images/next.png";
+
+import "./jukebox.css"
 
 class ArcadeMachine extends Component {
 	constructor(props) {
@@ -9,7 +14,8 @@ class ArcadeMachine extends Component {
 		this.state = {
 			trackIndex: 0,
 			height: window.innerHeight,
-			playing: false
+			playing: false,
+			hovering: ""
 		};
 	}
 
@@ -31,17 +37,45 @@ class ArcadeMachine extends Component {
 		}
 	}
 
+	changeHover = name => this.setState({ hovering: name })
+
+	myTurn = name => {
+		if (name === this.state.hovering) return "button-hover"
+			return "none"
+	}
+
 	render() {
-		const { height } = this.state
+		const { height, hovering } = this.state
 
 		return (
-	    <ImageMapper 
-	    	src={frontView} 
-	    	map={buttonCoords} 
-	    	height={0.95*height}
-	    	onMouseMove={x => null}
-	    	onClick={evt => this.buttonClick(evt.name)}
-	    />
+			<div>
+		    <ImageMapper 
+		    	src={frontView} 
+		    	map={buttonCoords} 
+		    	height={0.95*height}
+		    	// everything breaks without this function below, blame react-image-mapper
+		    	onMouseMove={x => null}
+		    	// onMouseEnter={evt => this.changeHover(evt.name)}
+		    	// onMouseLeave={() => this.changeHover("")}
+		    	onClick={evt => this.buttonClick(evt.name)}
+		    	onImageMouseMove={evt => {
+		    		const x = evt.clientX
+		    		const y = evt.clientY
+		    		if (357 <= x && x <= 378 && 543 <= y && y<= 565) {
+		    			console.log('pause')
+		    			this.changeHover("pause")
+		    		} else if (hovering !== "") {
+		    			console.log('none')
+		    			this.changeHover("")
+		    		}
+		    	}}
+		    />
+	    	<img 
+	    		src={pause} 
+	    		alt="pause" 
+	    		className={`pause ${this.myTurn("pause")}`}
+	    	/>
+	    </div>
 		);
 	}
 }
