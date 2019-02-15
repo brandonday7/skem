1f9f1e630e1be2f0e5b1a111e48353ec1b9d50b0
Brandon Day <brandontday7@gmail.com>
2019-02-14 21:57:53 -0500

    started carousel setup, switch between children with click on arrows

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import styled from 'styled-components'
+
+import next from "../../images/next.png"
+
+const StyledCarousel = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+const Left = styled.img`
+	position: absolute;
+	width: 60px;
+	transform: translateX(-47.5vw) scaleX(-1);
+	cursor: pointer;
+`
+
+const Right = styled.img`
+	position: absolute;
+	width: 60px;
+	transform: translateX(47.5vw);
+	cursor: pointer;
+`
+
+
+class Carousel extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			childIndex: 0,
+			lastChildIndex: this.props.children.length - 1
+		}
+	}
+
+	toTheLeft = () => {
+		const { childIndex, lastChildIndex } = this.state
+		if (childIndex !== lastChildIndex) {
+			this.setState({ childIndex: childIndex + 1 })
+		} else {
+			this.setState({ childIndex: 0 })
+		}
+	}
+
+	toTheRight = () => {
+		const { childIndex, lastChildIndex } = this.state
+		if (childIndex !== 0) {
+			this.setState({ childIndex: childIndex - 1 })
+		} else {
+			this.setState({ childIndex: lastChildIndex })
+		}
+	}
+
+	render() {
+		const { children } = this.props
+		const { childIndex } = this.state
+		return (
+			<StyledCarousel>
+				<Left src={next} onClick={this.toTheLeft} />
+				{children[childIndex]}
+				<Right src={next} onClick={this.toTheRight} />
+			</StyledCarousel>
+		);
+	}
+}
+
+export default Carousel;
--- src/components/Lyrics/index.js
+++ src/components/Lyrics/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import Carousel from "../Carousel";
 import DramaQueen from "../DramaQueen";
 import DependsOnYou from "../DependsOnYou";
 import SoMuchOfYourself from "../SoMuchOfYourself";
@@ -9,21 +8,12 @@ import "./carousel.css";
 class Lyrics extends Component {
 	render() {
 		return (
-			<Carousel
-				showThumbs={false}
-				showStatus={false}
-				showIndicators={false}
-				// swipeable={false}
-				infiniteLoop={true}
-			>
+			<Carousel>
 				<div>
-					<DramaQueen />
+					<p>HEY</p>
 				</div>
 				<div>
-					<DependsOnYou />
-				</div>
-				<div>
-					<SoMuchOfYourself />
+					<p>YO</p>
 				</div>
 			</Carousel>
 		);
