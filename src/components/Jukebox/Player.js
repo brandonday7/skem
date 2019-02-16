a174e54ac06b9c154cda5a49f73f3ac7b8dea50b
Brandon Day <brandontday7@gmail.com>
2019-02-16 17:03:43 -0500

    replaced tracks with youtube videos for disguise lol

--- src/components/Jukebox/Player.js
+++ src/components/Jukebox/Player.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import ReactPlayer from 'react-player'
 
-import intro from "../../songs/intro.mp3"
-import dq from "../../songs/dq.mp3"
-import dpoy from "../../songs/dpoy.mp3"
-import smoy from "../../songs/smoy.mp3"
+// import intro from "../../songs/intro.mp3"
+// import dq from "../../songs/dq.mp3"
+// import dpoy from "../../songs/dpoy.mp3"
+// import smoy from "../../songs/smoy.mp3"
 
 class Player extends Component {
 	constructor(props) {
@@ -13,19 +13,19 @@ class Player extends Component {
 			tracks: [
 				{
 					name: "intro",
-					src: intro
+					src: "https://www.youtube.com/watch?v=e2u27X4j6lo"
 				},
 				{
 					name: "dramaQueen",
-					src: dq
+					src: "https://www.youtube.com/watch?v=XKQNJzquduI"
 				},
 				{
 					name: "dependsOnYou",
-					src: dpoy
+					src: "https://www.youtube.com/watch?v=1ynHh8WgkIs"
 				},
 				{
 					name: "soMuchOfYourself",
-					src: smoy
+					src: "https://www.youtube.com/watch?v=5qKP32GYiks"
 				},
 			]
 		};
