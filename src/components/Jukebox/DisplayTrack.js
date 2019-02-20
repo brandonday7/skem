aea3c661aa550ad1e035a4664dc7102d7c4509da
Brandon Day <brandontday7@gmail.com>
2019-02-19 22:57:25 -0500

    media query for track title display, need to find a better way to put this in the center of the screen

--- src/components/Jukebox/DisplayTrack.js
+++ src/components/Jukebox/DisplayTrack.js
@@ -1,10 +1,6 @@
 import React from "react"
 import styled, { keyframes } from "styled-components"
 
-import dqTitle from "../../images/dqTitle.png"
-import dpoyTitle from "../../images/dpoyTitle.png"
-import smoyTitle from "../../images/smoyTitle.png"
-
 const fade = keyframes`
 	from {
 		opacity: 0;
@@ -18,16 +14,20 @@ const fade = keyframes`
 const StyledImage = styled.img`
 	width: 130px;
 	position: absolute;
-	transform: translate(10.2em, -25em);
+	transform: translate(10em, -25em);
 	z-index: 10;
 	animation: ${fade} 0.7s;
+
+  @media (max-width: 700px) {
+		transform: translate(40vw, -57vh);
+  }
 `
 
 const DisplayTrack = ({ trackIndex, playing }) => (
 	<div>
-		{trackIndex === 1 && <StyledImage src={dqTitle} />}
-		{trackIndex === 2 && <StyledImage src={dpoyTitle} />}
-		{trackIndex === 3 && <StyledImage src={smoyTitle} />}
+		{trackIndex === 1 && <StyledImage src={`${process.env.REACT_APP_BUCKET}dqTitle.png`} />}
+		{trackIndex === 2 && <StyledImage src={`${process.env.REACT_APP_BUCKET}dpoyTitle.png`} />}
+		{trackIndex === 3 && <StyledImage src={`${process.env.REACT_APP_BUCKET}smoyTitle.png`} />}
 	</div>
 )
 
