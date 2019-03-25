cd983fa9e7d77f603b350a598a10930977059ed8
Brandon Day <brandontday7@gmail.com>
2019-03-24 20:02:09 -0400

    added intro tag and press play, press play is temp

--- src/components/Jukebox/DisplayTrack.js
+++ src/components/Jukebox/DisplayTrack.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled, { keyframes } from "styled-components"
+import styled, { keyframes, css } from "styled-components"
 
 const fade = keyframes`
 	from {
@@ -11,36 +11,69 @@ const fade = keyframes`
 	}
 `
 
+const blink = keyframes`
+	0% {
+		opacity: 0;
+	}
+
+	50% {
+		opacity: 1;
+	}
+
+	100% {
+		opacity: 0;
+	}
+`
+
 const StyledImage = styled.img`
 	width: 20vh;
 	height: auto;
 	position: absolute;
-	transform: translate(${({ width }) => 0.35*width}px, -${({ height }) => 0.62*height}px);
+	transform: translate(${({ width }) => 0.36*width}px, -${({ height }) => 0.63*height}px);
 	z-index: 1;
-	animation: ${fade} 0.7s;
+	animation: ${({ blinking }) => {
+		if (blinking) {
+			return css`${blink} 2.5s infinite`
+		} else {
+			return css`${fade} 0.7s`
+		}
+	}}
 `
 
 const DisplayTrack = ({ trackIndex, playing, height, width }) => (
 	<div>
-		{trackIndex === 1 && (
+		{!playing && (
+			<StyledImage 
+				blinking
+				height={height} 
+				width={width} 
+				src={`${process.env.REACT_APP_BUCKET}pressPlaySmall.png`} 
+			/>)}
+		{!trackIndex && playing && (
+			<StyledImage 
+				height={height} 
+				width={width} 
+				src={`${process.env.REACT_APP_BUCKET}introTitleSmall.png`} 
+			/>)}
+		{trackIndex === 1 && playing && (
 			<StyledImage 
 				height={height} 
 				width={width} 
 				src={`${process.env.REACT_APP_BUCKET}dqTitleSmall.png`} 
 			/>)}
-		{trackIndex === 2 && (
+		{trackIndex === 2 && playing && (
 			<StyledImage 
 				height={height} 
 				width={width} 
 				src={`${process.env.REACT_APP_BUCKET}dpoyTitleSmall.png`} 
 			/>)}
-		{trackIndex === 3 && (
+		{trackIndex === 3 && playing && (
 			<StyledImage 
 				height={height} 
 				width={width} 
 				src={`${process.env.REACT_APP_BUCKET}smoyTitleSmall.png`} 
 			/>)}
-	</div>
+		</div>
 )
 
 export default DisplayTrack
