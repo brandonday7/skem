590018b55324884b0046c0151986ac0d0eccc847
Brandon Day <brandontday7@gmail.com>
2019-04-20 17:12:44 -0400

    added actual tracks to s3 bucket and ready to be commented out and played on release

--- src/components/Jukebox/Player.js
+++ src/components/Jukebox/Player.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import YouTubePlayer from 'react-player/lib/players/YouTube'
 import ReactPlayer from "react-player"
 
 class Player extends Component {
@@ -10,23 +9,22 @@ class Player extends Component {
 				{
 					name: "intro",
 					src: `${process.env.REACT_APP_BUCKET}Super+Rich+Kids.mp3`
-					// src: "https://www.youtube.com/watch?v=XIUQeSOFm0M"
-
+					// src: `${process.env.REACT_APP_BUCKET}mp3s/INTRO_FINAL.mp3`
 				},
 				{
 					name: "dramaQueen",
 					src: `${process.env.REACT_APP_BUCKET}tyler.mp3`
-					// src: "https://www.youtube.com/watch?v=SLrqm8F7TgM"
+					// src: `${process.env.REACT_APP_BUCKET}mp3s/DQ_FINAL.mp3`
 				},
 				{
 					name: "dependsOnYou",
 					src: `${process.env.REACT_APP_BUCKET}Super+Rich+Kids.mp3`
-					// src: "https://www.youtube.com/watch?v=XKQNJzquduI"
+					// src: `${process.env.REACT_APP_BUCKET}mp3s/DPOY_FINAL.mp3`
 				},
 				{
 					name: "soMuchOfYourself",
 					src: `${process.env.REACT_APP_BUCKET}tyler.mp3`
-					// src: "https://www.youtube.com/watch?v=muU_ERWCgJA"
+					// src: `${process.env.REACT_APP_BUCKET}mp3s/SMOY_FINAL.mp3`
 				},
 			]
 		};
