75b1c6b6da5be2a58012e953433479b2fdc3b407
Brandon Day <brandontday7@gmail.com>
2020-01-24 22:54:45 -0500

    figured out timing and sizing for i miss statement

--- src/components/IMissSummer/IMissStatements.js
+++ src/components/IMissSummer/IMissStatements.js
@@ -3,10 +3,11 @@ import styled from "styled-components"
 import "./i-miss-summer.css"
 
 const StyledMessage = styled.h1`
-	font-size: 500%;
+	font-size: ${({ size }) => size ? size : 500}%;
 	color: #D6A149;
 	-webkit-text-stroke: 1px #C3813E;
 	text-align: center;
+
 `
 
 const StyledFullscreen = styled.div`
@@ -27,67 +28,88 @@ class IMissStatements extends Component {
 			statements: [
 				{
 					message: "I miss",
-					time: 500
+					time: 500,
+					size: 800,
 				},
 				{
 					message: "being a kid",
-					time: 400
+					time: 400,
+					size: 500,
 				},
 				{
 					message: "I miss",
-					time: 400
+					time: 400,
+					size: 900,
 				},
 				{
 					message: "the sun",
-					time: 500
+					time: 500,
+					size: 450,
 				},
 				{
 					message: "I miss",
-					time: 600
+					time: 600,
+					size: 800,
 				},
 				{
 					message: "my friends",
-					time: 400
+					time: 400,
+					size: 500,
 				},
 				{
 					message: "I miss",
-					time: 400
+					time: 400,
+					size: 800,
 				},
 				{
 					message: "girls",
-					time: 200
+					time: 200,
+					size: 300,
 				},
 				{
 					message: "I miss",
-					time: 500
+					time: 500,
+					size: 500,
 				},
 				{
 					message: "when feeling close",
-					time: 800
+					time: 900,
+					size: 700,
 				},
 				{
 					message: "with my family",
-					time: 500
+					time: 500,
+					size: 400,
 				},
 				{
 					message: "didn't feel like an accomplishment",
-					time: 700
+					time: 700,
+					size: 500,
 				},
 				{
 					message: "I miss",
-					time: 300
+					time: 300,
+					size: 800,
 				},
 				{
 					message: "feeling like",
-					time: 200
+					time: 200,
+					size: 600,
 				},
 				{
 					message: "I was getting somewhere,",
-					time: 500
+					time: 500,
+					size: 500,
 				},
 				{
-					message: "and when somewhere had a name.",
-					time: 1300
+					message: "and when somewhere had a name",
+					time: 800,
+					size: 400,
+				},
+				{
+					message: "I MISS SUMMER",
+					time: 1300,
+					size: 800,
 				}
 			]
 		}
@@ -106,12 +128,7 @@ class IMissStatements extends Component {
 					this.setNewMessage()
 				})
 			}, statements[index].time)
-		} else {
-			setTimeout(() => {
-				this.setState({ complete: true })
-			}, statements[index].time)
-		}
-
+		} 
 	}
 
 
@@ -120,7 +137,11 @@ class IMissStatements extends Component {
 		const { complete, statements, index } = this.state
 		return (
 			<StyledFullscreen>
-				<StyledMessage>{complete ? "I Miss Summer" : statements[index].message}</StyledMessage>
+				<StyledMessage
+					size={statements[index].size} 
+				>
+					{statements[index].message}
+				</StyledMessage>
 			</StyledFullscreen>
 		)
 	}
